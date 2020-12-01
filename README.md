ac2cbb5bbc653bccda8a86447684f32980dd2b68
xfabregas <xfabregas@gmail.com>
2020-12-01 07:12:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-jpa753
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-jpa753)
---
+++ index.js
@@ -0,0 +1,9 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const btn = document.getElementById("b1");
+
+btn.onclick = () => {
+  alert("1");
+};
